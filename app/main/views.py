c386f787fba5f26c4b7ceb5922c3f034e54f6b21
FestusMutie <mutiefestus84@gmail.com>
2021-06-16 11:25:13 +0300

    update views.py file

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,30 @@
-from flask import render_template, redirect, url_for,abort,request
+from flask import render_template,request,redirect,url_for
 from . import main
-from flask_login import login_required,current_user
-from ..models import User,Pitch,Comment,Upvote,Downvote
-from .form import UpdateProfile,PitchForm,CommentForm
-from .. import db,photos
+from ..requests import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from ..models import Review
 
+# Views
 @main.route('/')
 def index():
-    movies = Pitch.query.all()
-    job = Pitch.query.filter_by(category = 'Job').all() 
-    event = Pitch.query.filter_by(category = 'Events').all()
-    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
-    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
+    movies = Movie.query.all()
+    action = Movie.query.filter_by(category = "action").all()
+    comedy = Movie.query.filter_by(category = "comedy").all() 
+    drama = Movie.query.filter_by(category = "drama").all()
+    horror = Movie.query.filter_by(category = "horror").all()
+    return render_template('index.html', action=action, comedy=comedy ,drama=drama,horror=horror)
 
-
+@main.route('/create_new', methods=['POST', 'GET'])
+@login_required
+def new_movie():
+    form = MovieForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user
+        new_movie_object = Movie(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_movie_object.save_p()
+        return redirect(url_for('main.index'))
+        
+    return render_template('create_movie.html', form = form)
