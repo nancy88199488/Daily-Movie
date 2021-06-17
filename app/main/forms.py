d56cd129d3f8464a16303affa757c05fb8632333
FestusMutie <mutiefestus84@gmail.com>
2021-06-17 20:32:53 +0300

    update forms.py file

---
+++ app/main/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write a brief bio about you.',validators = [Required()])
+    submit = SubmitField('Save')
+
+class SubscribeForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Action','Action'),('Comedy','Comedy'),('Drama','Drama')('Horror','Horror')],validators=[Required()])
+    submit = SubmitField('Movie')
+
