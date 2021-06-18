aba0583d44d9aeb662bc61a36782128ea47c7c15
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-18 05:37:19 +0300

    updating requirements file

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 mail = Mail
 login_manager = LoginManager()
@@ -12,6 +13,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 
 
 def create_app(config_name):
@@ -38,7 +40,8 @@ def create_app(config_name):
     # setting config
     from .requests import configure_request
     configure_request(app)
+
+        # configure UploadSet
+    configure_uploads(app,photos)
   
-
-
     return app
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export MOVIE_API_KEY=0b2773cd1fc8f86252f707f5c9b61dd1
 export SECRET_KEY=b'\xb5i\xbf\x95\xabd\x89T/\xfb(/\x9d\xbeK\xd6\x1a\xcaN\xc9I\xec\xea\xfc'
 
-python3.6 manage.py server
+python3.8 manage.py server
